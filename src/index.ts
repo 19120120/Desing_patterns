f9183381237ad5deede323d2af03f81ce2d9b449
Daniel Gutierrez Gomez <92828953+19120120@users.noreply.github.com>
2022-11-12 11:50:26 -0600

    robert

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import {Client} from 'pg';
 
 require("dotenv").config({
   path: ".env.local",
@@ -8,7 +9,20 @@ const app = express();
 const port = process.env.PORT;
 
 app.get("/", async (req, res) => {
-  res.send("Hello world! ganas");
+  
+  const client = new Client({
+    user: 'username',
+    host: '127.0.0.1',
+    database: 'test',
+    password: 'password',
+    port: 5433,
+  })
+  await client.connect()
+  const resdb = await client.query('SELECT NOW()')
+  await client.end()
+  console.log(resdb.rows);
+
+  res.send("Hello world! Este es de mi repos");
 });
 
 app.listen(port, () => {
